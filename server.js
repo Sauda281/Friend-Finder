417c551c1dc461e0b7b027fcda4c364d9b88a73d
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-04-04 12:57:13 -0500

    changes

--- server.js
+++ server.js
@@ -10,8 +10,8 @@
 
  app.use('/static', express.static(path.join(__dirname, 'app/public')))
 
- require('./app/routing/apiRoutes.js')(app);
- require('./app/routing/htmlRoutes.js')(app);
+ require('./app/routing/apiRoutes')(app);
+ require('./app/routing/htmlRoutes')(app);
 
  app.listen(PORT, function(){
      console.log("App listening on PORT: " + PORT);
